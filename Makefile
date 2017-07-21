e4b07c767236f5dabcadb2b7219d1da775c02e1e
marc-despland <marc.despland@art122-5.net>
2017-07-21 18:43:30 +0200

    initialize the daemon project

---
+++ Makefile
@@ -0,0 +1,62 @@
+SHELL = /bin/sh
+CC    = g++
+
+CPPFLAGS       = -g -Wall -Iinclude 
+OLDFLAGS	= -std=c++11
+CFLAGS       = $(CPPFLAGS) 
+LDLIBS		 = -lutil -lbluetooth -lwiringPi
+TARGET  = alarm
+SOURCES = $(shell echo src/*.cpp)
+HEADERS = $(shell echo include/*.h)
+OBJECTS = $(SOURCES:.cpp=.o)
+MAIN	= $(shell echo main/*.cpp)
+MAINOBJ	= $(MAIN:.cpp=.o)
+DEBUGFLAGS = -g
+ 
+all: $(TARGET)
+
+install: $(TARGET)
+	- mkdir -p /opt/alarm/bin
+	- mkdir -p /opt/alarm/etc
+	- mkdir -p /opt/alarm/log
+	- mkdir -p /opt/alarm/run
+	- cp alarm /opt/alarm/bin/
+	- chmod a+rx /opt/alarm/bin/alarm
+	- cd /opt/alarm/bin; ./alarm -a create -f /opt/alarm/etc/alarm.conf
+	- cp etc/alarm /etc/init.d/
+	- chmod a+rx /etc/init.d/alarm
+
+clean:
+	-echo "clean"
+	-rm -f $(OBJECTS)
+	-rm -f gmon.out
+	-rm -f main/*.o
+	-rm -f $(TARGET)
+ 
+distclean: clean
+	-rm -f $(TARGET)
+ 
+gitadd:
+	-git add src/*.cpp
+	-git add include/*.h
+	-git add main/*.cpp
+	-git add Makefile
+ 
+.SECONDARY: $(OBJECTS) $(MAINOBJ)
+.PHONY : all install uninstall clean distclean gitadd
+
+.SECONDEXPANSION:
+
+$(foreach TGT, $(TARGET), $(eval $(TGT)_HEADERS = $(filter $(shell $(CC) $(CFLAGS) -MM main/$(TGT).cpp | sed s/.*://), $(HEADERS))))
+$(foreach TGT, $(TARGET), $(eval $(TGT)_OBJECTS = $(filter $(subst include, src, $($(TGT)_HEADERS:.h=.o)), $(OBJECTS))))
+$(foreach OBJ,$(OBJECTS),$(eval $(OBJ)_HEADERS = $(filter $(shell $(CC) $(CFLAGS) -MM $(OBJ:.o=.cpp) | sed s/.*://), $(HEADERS))))
+$(foreach OBJ,$(OBJECTS),$(eval $(OBJ)_OBJECTS = $(filter-out $(OBJ), $(filter $(subst include, src, $($(OBJ)_HEADERS:.h=.o)), $(OBJECTS)))))
+
+
+%:   main/%.o $$($$@_OBJECTS)  $$($$@_HEADERS)
+	$(CC) $(CFLAGS) -o $@ $($@_OBJECTS) $< $(LDLIBS)
+ 
+%.o: %.cpp $$($$@_OBJECTS) $$($$@_HEADERS)
+	$(CC) $(CFLAGS) $(LDLIBS) -c -o $@ $<
+
+
---
+++ main/alarm.cpp
@@ -0,0 +1,91 @@
+#include "daemon.h"
+#include "log.h"
+#include "blueping.h"
+#include "ping.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <wiringPi.h>
+#include <time.h>
+
+#define LED_PIN 1
+#define PIR_PIN 0
+
+class AlarmDaemon:public Daemon {
+	public:
+		static void Initialize(string program, string version, string description) {
+			if (Daemon::me==NULL) {
+				Daemon::me=new AlarmDaemon(program, version, description);
+			}
+		}
+		~AlarmDaemon() {
+		}
+	protected :
+		int lastevent;
+		bool monitor;
+		bool stateon;
+		AlarmDaemon(string program, string version, string description):Daemon(program, version, description) {
+			try {
+				Log::logger->log("MAIN",NOTICE) << "Adding program parameters descriptions" << endl;
+			} catch(ExistingParameterNameException &e ) {
+				Log::logger->log("MAIN", EMERGENCY) << "Can't create one of the file parameters"<< endl;
+			}
+		}
+
+		void daemon(){
+			Log::logger->log("MAIN",NOTICE) << "Child daemon started" << endl;
+			this->monitor=true;
+			this->stateon=false;
+			if (wiringPiSetup () < 0) {
+				Log::logger->log("MAIN",ERROR) << "Failed to initialize wiringPi librairies: " << strerror (errno)) << endl;
+				this->monitor=false;
+			} else {
+				this->lastevent=(int) time(NULL);
+				pinMode (PIR_PIN, INPUT) ; 
+				pinMode (LED_PIN, OUTPUT);
+				digitalWrite(LED_PIN, 0);
+				if ( wiringPiISR (PIR_PIN, INT_EDGE_BOTH, &AlarmDaemon::Intrusion) < 0 ) {
+					Log::logger->log("MAIN",ERROR) << "Unable to setup ISR: " << strerror (errno)) << endl;
+					this->monitor=false;
+				}
+			}
+			while (this->monitor) {
+				int now=(int) time(NULL);
+				if (lastevent+10<now) {
+					digitalWrite(LED_PIN, 0);
+				}	
+				delay( 1000 ); 
+			}		
+		}
+		void terminate(){
+			Log::logger->log("MAIN",NOTICE) << "Child daemon terminate" << endl;
+			this->monitor=false;
+			::system(this->parameters->get("stop")->asChars());
+		}
+		static void Intrusion(void) {
+			AlarmDaemon * me=(AlarmDaemon *) Daemon::me;
+			me->lastevent=(int) time(NULL);
+			digitalWrite(LED_PIN, 1);
+		}	
+};
+
+
+
+
+int main(int argc, char **argv) {
+	Log::logger->setLevel(DEBUG);
+	AlarmDaemon::Initialize(argv[0], "1.0.0", "Alarm monitor with PIR mouvement detector");
+		//exit(0);
+
+	try {
+		AlarmDaemon::Start(argc, argv);
+	} catch(ForkException &e) {
+		Log::logger->log("MAIN",NOTICE) << "ForkException occurs" << endl;
+	} catch(OptionsStopException &e) {
+		Log::logger->log("MAIN",NOTICE) << "OptionsStopException occurs" << endl;
+	} catch(UnknownOptionException &e) {
+		Log::logger->log("MAIN",NOTICE) << "UnknownException occurs" << endl;
+	} catch(CantCreateFileException &e) {
+		Log::logger->log("MAIN",NOTICE) << "CantCreateFileException occurs" << endl;
+	}
+}
